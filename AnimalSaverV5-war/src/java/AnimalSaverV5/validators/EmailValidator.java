d35e38acaa94a27b9a94252a1223c4874664cf12
ridwan <ridwan_rahman@ymail.com>
2019-10-06 13:26:34 +1100

    Added custom validator

---
+++ AnimalSaverV5-war/src/java/AnimalSaverV5/validators/EmailValidator.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AnimalSaverV5.validators;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.html.HtmlInputText;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ *
+ * @author ridwanurrahman
+ */
+@FacesValidator(value = "emailValidator")
+public class EmailValidator implements Validator {
+    private static final String URL_REGEX =
+			"^((((https?|ftps?|gopher|telnet|nntp)://)|(mailto:|news:))" +
+			"(%[0-9A-Fa-f]{2}|[-()_.!~*';/?:@&=+$,A-Za-z0-9])+)" +
+			"([).!';/?:,][[:blank:]])?$";
+    
+    private static final Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
+    
+    public static boolean urlValidator(String url) {
+        if (url == null) {
+            return false;
+        }
+        Matcher matcher = URL_PATTERN.matcher(url);
+        return matcher.matches();
+    }
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        System.out.println("hererer");
+        StringBuilder url = new StringBuilder();
+        String urlValue = value.toString();
+        if (urlValidator(urlValue)) {
+            System.out.print("The URL " + url + " is valid");
+//            context = FacesContext.getCurrentInstance();
+//            context.addMessage(component.getClientId(), new FacesMessage("URL is fine"));
+        } else {
+            System.out.print("The URL " + url + " isn't valid");
+            context = FacesContext.getCurrentInstance();
+            FacesMessage msg = new FacesMessage("URL validation failed","Invalid URL format");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            context.addMessage(component.getClientId(), msg);
+        }
+    }
+}
